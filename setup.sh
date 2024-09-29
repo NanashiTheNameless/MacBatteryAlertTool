ed9f19b522a70139046f3e18e2e58d3f8d3aa610
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2024-09-29 01:15:23 -0500

    slight tweak to contained script

--- setup.sh
+++ setup.sh
@@ -20,6 +20,9 @@ cat << 'EOF' > "$SCRIPT_PATH"
 timeInbetweenAlerts=30
 batteryPercentAlert=15
 
+# This is from https://github.com/NanashiTheNameless/MacBatteryAlertTool
+# It is licensed under https://github.com/NanashiTheNameless/MacBatteryAlertTool/blob/main/license.md
+
 while true; do
     if pmset -g batt | head -n 1 | grep -q "Battery" && [ $(pmset -g batt | grep -o '[0-9]\{1,3\}%' | tr -d '%') -le $batteryPercentAlert ]; then
         afplay /System/Library/Sounds/Funk.aiff
